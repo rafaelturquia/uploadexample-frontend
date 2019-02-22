f689404186f0332f8a6500c596c0e297cd9ba318
rafaelturquia <rafaelturquia@gmail.com>
2019-02-22 18:51:35 -0300

    configuration upload from file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
 
 import GlobalStyle from './styles/global';
 import { Container, Content } from './styles';
@@ -12,15 +14,33 @@ class App extends Component {
   };
 
   handleUpload = files => {
-    console.log(files);
+    const uploadedFiles = files.map( file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0,
+      uploaded: false,
+      error: false,
+      url: null,
+    }))
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
   };
 
   render() {
+    const { uploadedFiles } = this.state;
+
     return (
       <Container>
         <Content>
-          <Upload />
-          <FileList />
+          <Upload onUpload={this.handleUpload}/>
+          { !!uploadedFiles.length && (
+            <FileList files={uploadedFiles} />
+          ) }
         </Content>
         <GlobalStyle />
       </Container>
--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -4,39 +4,47 @@ import { MdCheckCircle, MdError, MdLink } from 'react-icons/md'
 
 import { Container, FileInfo, Preview } from './styles';
 
-const FileList = () => (
+const FileList = ({ files }) => (
   <Container>
-    <li>
+    {files.map(uploadedFile => (
+      <li>
       <FileInfo>
-        <Preview src="http://localhost:3000/files/1ded785592ff172b101835d1e9c5cde9-IMG_4216.JPG">
+        <Preview src={uploadedFile.preview}>
           <div>
-            <strong></strong>
-            <span>64kb <button onClick={() => {}}>Excluir</button></span>
+            <strong>{uploadedFile.name}</strong>
+            <span>
+              {uploadedFile.readableSize} <button onClick={() => {}}>Excluir</button>
+            </span>
           </div>
         </Preview>
         </FileInfo>
         <div>
-          <CircularProgressbar
+          {!uploadedFile.uploaded && !uploadedFile.error && (
+            <CircularProgressbar
             styles={{
               root: { width: 24},
               path: { stroke: '#7159c1'}
             }}
             strokeWidth={10}
-            percentage={60}
+            percentage={uploadedFile.progress}
           />
+          )}
 
-          <a
+          {uploadedFile.url && (
+            <a
             href="http://localhost:3000/files/1ded785592ff172b101835d1e9c5cde9-IMG_4216.JPG"
             target="_blank"
             rel="noopener noreferrer"
           >
             <MdLink style={{ marginRight: 8}} size={24} color="#222" />
           </a>
+          )}
 
-          <MdCheckCircle size={24} color="#78e5d5" />
-          <MdError size={24} color="#e57878" />
+          {uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+          {uploadedFile.error && <MdError size={24} color="#e57878" />}
         </div>
     </li>
+    ))}
   </Container>
 );
 
